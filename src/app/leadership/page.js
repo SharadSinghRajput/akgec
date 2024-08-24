5724872077d1b45a88e17b714a493deb23ea0b62
Ashish Verma <ashishverma@Ashishs-Mac-mini.local>
2024-08-24 15:44:25 +0530

    Changes in horoscope hit function

---
+++ src/app/leadership/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Menu from "../Components/NavBar"
+import Footer from '../Components/Footer';
+
+export default function Home() {
+  return (
+    <>
+    <div className="bg-red-500 w-full h-screen">
+      
+    </div>
+    </> 
+  );
+}
