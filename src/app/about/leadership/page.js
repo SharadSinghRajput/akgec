2e49fc741272093750d8484a6c004f488abdbafc
Ashish Verma <ashishverma@Ashishs-Mac-mini.local>
2024-08-28 13:09:39 +0530

    New code

--- src/app/about/leadership/page.js
+++ src/app/about/leadership/page.js
@@ -1,6 +1,7 @@
 import Directors from './Directors';
 import Header from '../../Components/Header';
 import { AboutSideBar } from '../../Components/AboutSideBar';
+import Breadcrumb from '../../Components/Breadcrumb';
 
 const ButtoName = {
   name: "Apply Now",
@@ -23,14 +24,7 @@ export default function Home() {
     <>
       <Header title={"Leadership"} />
       <div className="max-w-[1400px] w-full mx-auto pt-16 p-5">
-        <ul className='flex justify-start gap-2'>
-          <li className='uppercase text-xs'>Home</li>
-          {BreadCrumb.map((item, index)=>(
-            <li key={index}  className={`before:content-['/'] before:pr-2 uppercase text-xs ${
-              index === BreadCrumb.length - 1 ? 'font-semibold' : ''
-            }`}>{item.name}</li>
-          ))}
-        </ul>
+        <Breadcrumb data={BreadCrumb} />
         <div className="flex flex-col lg:flex-row flex-1 gap-5 mt-5">
           <div className="flex-1">
             <Directors />
--- src/app/about/management/Management.js
+++ src/app/about/management/Management.js
@@ -173,56 +173,63 @@ const committee = [
 
 export default function Management() {
     return (
-        <div className="py-24 sm:py-32 ">
-            <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                <div className="border-b-4 border-gray-300 pb-2">
-                    <h2 className="text-3xl font-bold text-center tracking-tight text-gray-700 sm:text-4xl">{governor[0].title}</h2>
+        <div className=" flex flex-col gap-10">
+            <div className="">
+                <div className="border-bmb-5 bg-blue-800 py-4 px-4 mb-5">
+                    <h2 className="text-base font-normal text-left tracking-tight text-white">{governor[0].title}</h2>
                 </div>
 
                 <ul
                     role="list"
-                    className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-14 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 xl:grid-cols-4"
+                    className="grid grid-cols-5 gap-4"
                 >
                     {governor.slice(1).map((person) => (
-                        <li key={person.name} className="border shadow-xl rounded-lg p-4 text-center">
-                            <img alt="" src={person.imageUrl} className="aspect-[14/13] w-full rounded-2xl object-contain" />
-                            <h3 className="mt-6 text-lg font-semibold text-gray-500 leading-8 tracking-tight">{person.name}</h3>
-                            <p className="text-base leading-7 text-gray-500">{person.role}</p>
+                        <li key={person.name} className="border border-gray-300 shadow-lg rounded-md">
+                            <img alt="" src={person.imageUrl} className="w-full aspect-square p-5 object-cover" />
+                            <div className="p-4">
+                                <h3 className="font-normal text-sm text-gray-900">{person.name}</h3>
+                                <p className="font-normal text-xs text-gray-800">{person.role}</p>
+                            </div>
 
                         </li>
                     ))}
                 </ul>
             </div>
-            <div className="mt-20 mx-auto max-w-7xl px-6 lg:px-8">
-                <div className="border-b-4 border-gray-300 pb-2">
-                    <h2 className="text-3xl font-bold text-center tracking-tight text-gray-700 sm:text-4xl">{council[0].title}</h2>
+
+            <div className="">
+                <div className="border-bmb-5 bg-blue-800 py-4 px-4 mb-5">
+                    <h2 className="text-xl font-normal text-left tracking-tight text-white">{council[0].title}</h2>
                 </div>
                 <ul
                     role="list"
-                    className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-14 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 xl:grid-cols-4"
+                    className="grid grid-cols-5 gap-4"
                 >
                     {council.slice(1).map((person) => (
-                        <li key={person.name} className="border shadow-xl rounded-lg p-4 text-center">
-                            <img alt="" src={person.imageUrl} className="aspect-[14/13] w-full rounded-2xl object-contain" />
-                            <h3 className="mt-6 text-lg font-semibold text-gray-600 tracking-tight">{person.name}</h3>
-                            <p className="text-base  text-gray-500">{person.role}</p>
+                      <li key={person.name} className="border border-gray-300 shadow-lg rounded-md">
+                            <img alt="" src={person.imageUrl} className="w-full aspect-square p-5 object-cover" />
+                            <div className="p-4">
+                                <h3 className="font-normal text-sm text-gray-900">{person.name}</h3>
+                                <p className="font-normal text-xs text-gray-800">{person.role}</p>
+                            </div>
                         </li>
                     ))}
                 </ul>
             </div>
-            <div className="mt-20 mx-auto max-w-7xl px-6 lg:px-8">
-                <div className="border-b-4 border-gray-300 pb-2">
-                    <h2 className="text-3xl font-bold text-center tracking-tight text-gray-700 sm:text-4xl">{committee[0].title}</h2>
+            <div className="">
+                <div className="border-bmb-5 bg-blue-800 py-4 px-4 mb-5">
+                    <h2 className="text-xl font-normal text-left tracking-tight text-white">{committee[0].title}</h2>
                 </div>
                 <ul
                     role="list"
-                    className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-14 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 xl:grid-cols-4"
+                    className="grid grid-cols-5 gap-4"
                 >
                     {committee.slice(1).map((person) => (
-                        <li key={person.name} className="border shadow-xl rounded-lg p-4 text-center">
-                            <img alt="" src={person.imageUrl} className="aspect-[14/13] w-full rounded-2xl object-contain" />
-                            <h3 className="mt-6 text-lg font-semibold text-gray-600 tracking-tight">{person.name}</h3>
-                            <p className="text-base  text-gray-500">{person.role}</p>
+                        <li key={person.name} className="border border-gray-300 shadow-lg rounded-md">
+                            <img alt="" src={person.imageUrl} className="w-full aspect-square p-5 object-cover" />
+                            <div className="p-4">
+                                <h3 className="font-normal text-sm text-gray-900">{person.name}</h3>
+                                <p className="font-normal text-xs text-gray-800">{person.role}</p>
+                            </div>
                         </li>
                     ))}
                 </ul>
