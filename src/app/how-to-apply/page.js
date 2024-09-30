0f2b9c8674213d1cd2ea188aae8138b08843f467
Ashish Verma <ashishverma@Ashishs-Mac-mini.local>
2024-09-30 10:16:50 +0530

    new changes

---
+++ src/app/how-to-apply/page.js
@@ -0,0 +1,38 @@
+import Header from '../../Components/Header';
+import SideBar from '@/Components/SideBar';
+import Breadcrumb from '../../Components/Breadcrumb';
+import { HowToApply } from './HowToApply';
+
+const ButtoName = {
+  name: "Apply Now",
+  Link: "",
+};
+const BreadCrumb = [
+  {
+    name: "about",
+    Link: "",
+  },
+  {
+    name: "Hostel",
+    Link: "about",
+  }
+]
+
+export default function Home() {
+  return (
+    <>
+      <Header title={"Hostel"} />
+      <div className="max-w-[1400px] w-full mx-auto pt-16 p-5">
+        <Breadcrumb data={BreadCrumb} />
+        <div className="flex flex-col lg:flex-row flex-1 gap-5 mt-5">
+          <div className="flex-1">
+            <HowToApply />
+          </div>
+          <div className="sm:w-1/4 ">
+            <SideBar />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
