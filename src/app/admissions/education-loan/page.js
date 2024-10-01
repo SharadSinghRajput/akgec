845d2d33307844523df12ad941853c30bc711562
atulsadiwal <atul.sadiwal@brandshow.in>
2024-10-01 15:12:37 +0530

    changed

---
+++ src/app/admissions/education-loan/page.js
@@ -0,0 +1,23 @@
+import Header from "@/Components/Header";
+import SideBar from "@/Components/SideBar";
+import EducationLoan from "./EducationLoan";
+
+
+
+export const Home = () => {
+    return (
+        <>
+            <div className="bg-gray-100">
+                <Header title={<span className="leading-[45px] flex pl-10 ">Study Loan Assistance <br /> for AKG Student</span>} gradient={"bg-gradient-to-r from-gray-900 to-gray-900/40"} />
+                <section className="w-full max-w-[1400px] mx-auto grid grid-cols-12 py-10 gap-10 px-6 max-md:px-5">
+                    <div className="col-span-12 max-md:col-span-12 ">
+                        <EducationLoan />
+                    </div>
+                </section>
+            </div>
+        </>
+    )
+}
+
+
+export default Home;
