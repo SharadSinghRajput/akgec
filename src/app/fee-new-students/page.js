78fb210545d05b98c2afc053a4f962dddddff012
atulsadiwal <atul.sadiwal@brandshow.in>
2024-10-24 19:06:02 +0530

    changed

---
+++ src/app/fee-new-students/page.js
@@ -0,0 +1,39 @@
+import Header from "@/Components/Header";
+import FeeNewStudents from "./FeeNewStudents";
+import SideBar from "@/Components/SideBar";
+
+const Button = {
+    name: "Apply Now",
+    Link: "",
+};
+
+const SideBarLink = [
+    { name: "Our Identity", link: "/overview" },
+    { name: "Leadership", link: "" },
+    { name: "Leadership", link: "" },
+    { name: "Governance", link: "" },
+    { name: "Recognition and Approvals", link: "" },
+    { name: "Awards and Rankings", link: "" },
+    { name: "Institution Social Responsibility", link: "" }
+]
+
+const Home = () => {
+    return (
+        <>
+            <div className="bg-gray-100">
+                <Header title={"Fee Structure 2024-25"} Button={Button} gradient={"bg-gradient-to-r from-blue-900 to-blue-900/40"} />
+                <section className="w-full max-w-[1400px] mx-auto grid grid-cols-12 py-20 gap-10">
+                    <div className="col-span-9 max-md:col-span-12">
+                        <FeeNewStudents />
+                    </div>
+                    <div className="col-span-3 max-md:col-span-12">
+                        <SideBar title={"About Us"} LinkList={SideBarLink} />
+                    </div>
+                </section>
+            </div>
+        </>
+    )
+}
+
+
+export default Home;
--- src/app/nirf-data-for-ranking-2024/NIRFData.js
+++ src/app/nirf-data-for-ranking-2024/NIRFData.js
@@ -4,7 +4,7 @@ const NIRFData = () => {
   return (
     <section>
         <iframe
-        src="/pdf/mandatory-disclosure/NIRF-2024-Data.pdf" // Replace with the path to your PDF
+        src="/pdf/mandatory-disclosure/NIRF-2024-Data.pdf" 
         width="100%"
         height="600px"
         title="NIRF Data"
