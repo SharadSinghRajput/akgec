de164f4108c6e3918b0d9302bb70315e0d5fa2b1
atulsadiwal <atul.sadiwal@brandshow.in>
2024-11-22 10:43:17 +0530

    admin panel live

--- src/app/admin/edit-news-and-event/page.js
+++ src/app/admin/edit-news-and-event/page.js
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import SideBar from "../Components/SideBar";
 import EditNewsAndEvent from "./EditNewsAndEvent";
 
@@ -7,7 +8,9 @@ export default function Home() {
       <div className="flex gap-10 bg-gray-100">
         <SideBar />
         <div className="pt-16 overflow-x-auto w-full h-screen">
-          <EditNewsAndEvent />
+          <Suspense fallback={<div>Loading...</div>}>
+            <EditNewsAndEvent />
+          </Suspense>
         </div>
       </div>
     </>
