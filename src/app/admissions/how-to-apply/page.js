64773fa8fcaca9e00deb77899110a151dc222f92
JatinBrandshow <jatin.yadav@brandshow.in>
2025-02-13 11:32:57 +0530

    New Changes

--- src/app/admissions/how-to-apply/page.js
+++ src/app/admissions/how-to-apply/page.js
@@ -1,6 +1,7 @@
 import Header from '@/Components/Header';
 import SideBar from '@/Components/SideBar';
 import { HowToApply } from './HowToApply';
+import { Button } from '@/Components';
 
 
 const SideBarLink = [
@@ -16,7 +17,7 @@ export default function Home() {
   return (
     <>
       <div className="bg-gray-100">
-        <Header title={"How To Apply"} gradient={"bg-gradient-to-r from-gray-900 to-transparent"} />
+        <Header title={"How To Apply"} gradient={"bg-gradient-to-r from-gray-900 to-transparent"} Button={{ name: "Apply Now", Link: "/" }}/>
         <section className="w-full max-w-[1400px] mx-auto grid grid-cols-12 py-20 max-sm:py-5 gap-8 max-sm:gap-0 px-4 max-sm:px-2">
           <div className="col-span-9 max-xl:col-span-8 max-lg:col-span-12">
             <HowToApply />
